37cf988f28cf108ef464df6da5fa00d522fce992
AnTosi <andrea.tosi.uni@gmail.com>
2021-12-16 16:28:20 +0100

    do dynamic data in routes with compact(), foreach loop

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,5 +8,13 @@
 </head>
 <body>
     <h1>Hello World!</h1>
+    <p>I have {{ $number }} {{ $animals }}</p>
+    <h3>Si chiamano:</h3>
+    @foreach ($cats_name as $cat_name)
+        <p>
+            {{ $cat_name}}
+        </p>
+    @endforeach
+        
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
-});
+    $animals = 'gatti';
+    $number = '3';
+    $cats_name = [
+        'Dafne', 'Lenticchia', 'Jinx'
+    ];
+    return view('home', compact('animals', 'number', 'cats_name'));
+})->name('home');
